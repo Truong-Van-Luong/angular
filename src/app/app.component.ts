f712c4a7339b250a58e358923a2e6309d40772ae
Van Luong <truongluong28082808@gmail.com>
2024-05-19 20:43:33 +0700

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,97 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  products = [
+    {
+      price :  15.023,
+      priceSale : 25.019,
+      seller : 'Best Seller',
+      title : 'Bed Side Table ',
+      content : 'A beaautiful side table that will perfectly fit your lovely bed room.With space for your books, lamps and electronic devices',
+      instock : 'In Stock',
+      outstock : 'Out Stock',
+    },
+    {
+      price :  15.023,
+      priceSale : 25.019,
+      seller : 'Best Seller',
+      title : 'Bed Side Table',
+      content : 'A beaautiful side table that will perfectly fit your lovely bed room.With space for your books, lamps and electronic devices',
+      instock : 'In Stock',
+      outstock : 'Out Stock',
+    },
+    {
+      price :  15.023,
+      priceSale : 25.019,
+      seller : 'Best Seller',
+      title : 'Bed Side Table',
+      content : 'A beaautiful side table that will perfectly fit your lovely bed room.With space for your books, lamps and electronic devices',
+      instock : 'In Stock',
+      outstock : 'Out Stock',
+    },
+    {
+      price :  15.023,
+      priceSale : 25.019,
+      seller : 'Best Seller',
+      title : 'Bed Side Table',
+      content : 'A beaautiful side table that will perfectly fit your lovely bed room.With space for your books, lamps and electronic devices',
+      instock : 'In Stock',
+      outstock : 'Out Stock',
+    },
+    {
+      price :  15.023,
+      priceSale : 25.019,
+      seller : 'Best Seller',
+      title : 'Bed Side Table',
+      content : 'A beaautiful side table that will perfectly fit your lovely bed room.With space for your books, lamps and electronic devices',
+      instock : 'In Stock',
+      outstock : 'Out Stock',
+    },
+    {
+      price :  15.023,
+      priceSale : 25.019,
+      seller : 'Best Seller',
+      title : 'Bed Side Table',
+      content : 'A beaautiful side table that will perfectly fit your lovely bed room.With space for your books, lamps and electronic devices',
+      instock : 'In Stock',
+      outstock : 'Out Stock',
+    },
+    {
+      price :  15.023,
+      priceSale : 25.019,
+      seller : 'Best Seller',
+      title : 'Bed Side Table',
+      content : 'A beaautiful side table that will perfectly fit your lovely bed room.With space for your books, lamps and electronic devices',
+      instock : 'In Stock',
+      outstock : 'Out Stock',
+    },
+    {
+      price :  15.023,
+      priceSale : 25.019,
+      seller : 'Best Seller',
+      title : 'Bed Side Table',
+      content : 'A beaautiful side table that will perfectly fit your lovely bed room.With space for your books, lamps and electronic devices',
+      instock : 'In Stock',
+      outstock : 'Out Stock',
+    },
+    {
+      price :  15.023,
+      priceSale : 25.019,
+      seller : 'Best Seller',
+      title : 'Bed Side Table',
+      content : 'A beaautiful side table that will perfectly fit your lovely bed room.With space for your books, lamps and electronic devices',
+      instock : 'In Stock',
+      outstock : 'Out Stock',
+    },
+    {
+      price :  15.023,
+      priceSale : 25.019,
+      seller : 'Best Seller',
+      title : 'Bed Side Table',
+      content : 'A beaautiful side table that will perfectly fit your lovely bed room.With space for your books, lamps and electronic devices',
+      instock : 'In Stock',
+      outstock : 'Out Stock',
+    }
+]
+
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -6,17 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  product = {
-    number : '1/5',
-    price :  15.023,
-    priceSale : 25.019,
-    seller : 'Best Seller',
-    title : 'Bed Side Table',
-    content : 'A beaautiful side table that will perfectly fit your lovely bed room.With space for your books, lamps and electronic devices',
-    instock : 'In Stock',
-    outstock : 'Out Stock',
-  }
+  @Input() data!: any;
+  @Output() dataChange = new EventEmitter<any>();
 
+ 
   
   isBestSeller = false;
 
@@ -28,29 +21,12 @@ export class ProductComponent {
     { class: 'bg-info' },
     { class: 'bg-black' }
   ];
-  selectedColor = this.colors[0]; // Khởi tạo giá trị mặc định
+  selectedColor = this.colors[0]; 
+  
   // size
   sizes = ['45*53', '42*40', '40*40', '35*49'];
-  selectedSize = this.sizes[0]; // Khởi tạo giá trị mặc định
+  selectedSize = this.sizes[0]; 
 
   
-
-  //  activeColor (event: MouseEvent) {
-  //     const target = event.target as HTMLElement;
-  //     const element = document.querySelectorAll('.main-color');
-  //     element.forEach((item)=> {
-  //       item.classList.remove('active-color');
-  //     })
-  //     target.classList.add('active-color')
-  //  }
-
-//    activeSize (event: MouseEvent) {
-//     const target = event.target as HTMLElement;
-//     const element = document.querySelectorAll('.Size-home-pr');
-//     element.forEach((item)=> {
-//       item.classList.remove('black');
-//     })
-//     target.classList.add('black') 
-//  }
   }
   
